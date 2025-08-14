c50d5ed8f338bd6722dab3373ee8ea8d960ba009
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-08-14 07:29:31 +0000

    updated form_width with model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,15 +22,14 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params.fetch(:movie).fetch(:title)
-    @movie.description = params.fetch(:movie).fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
 
     if @movie.valid?
       @movie.save
       redirect_to(movies_url, { notice: "Movie was successfully created." })
     else
-      render template: "/movies/new"
+      render "new"
     end
   end
 
@@ -40,10 +39,8 @@ class MoviesController < ApplicationController
   end
 
   def update
-    movie = Movie.find(params.fetch(:id))
-
-    movie.title = params.fetch(:title)
-    movie.description = params.fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    movie = Movie.new(movie_attributes)
 
     if movie.valid?
       movie.save
